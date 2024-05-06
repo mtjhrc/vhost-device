4243afefbbc6aafd74a7a9415d8a95b14a816b3d
Matej Hrica <mhrica@redhat.com>
2024-05-06 10:20:25 +0200

    FIXME: Added a workaround in transfer_write
    This works around an issue with a guest image that uses transfer_write to
    updatea portion of the screen - such as a server image without GUI
    displaying a console. Without this workaround most of the image would be
    missing / the screen would update weirdly. GUI images that update the whole
    screen area at once where not effected by this issue.
    
    Signed-off-by: Matej Hrica <mhrica@redhat.com>

--- staging/vhost-device-gpu/src/virt_gpu.rs
+++ staging/vhost-device-gpu/src/virt_gpu.rs
@@ -7,7 +7,7 @@ use std::sync::{Arc, Mutex};
 
 use crate::vhu_gpu::Error;
 use libc::c_void;
-use log::{debug, error};
+use log::{debug, error, trace};
 use rutabaga_gfx::{
     ResourceCreate3D, ResourceCreateBlob, Rutabaga, RutabagaBuilder, RutabagaChannel,
     RutabagaFence, RutabagaFenceHandler, RutabagaIovec, RutabagaResult, Transfer3D,
@@ -316,6 +316,7 @@ impl VirtioGpuResource {
 
 pub struct VirtioGpuScanout {
     resource_id: u32,
+    rect: Rectangle,
 }
 
 pub struct RutabagaVirtioGpu {
@@ -589,7 +590,7 @@ impl VirtioGpu for RutabagaVirtioGpu {
             .ok_or(ErrInvalidResourceId)?;
 
         resource.scanouts.enable(scanout_id);
-        *scanout = Some(VirtioGpuScanout { resource_id });
+        *scanout = Some(VirtioGpuScanout { resource_id, rect });
         Ok(OkNoData)
     }
 
@@ -683,8 +684,26 @@ impl VirtioGpu for RutabagaVirtioGpu {
         &mut self,
         ctx_id: u32,
         resource_id: u32,
-        transfer: Transfer3D,
+        mut transfer: Transfer3D,
     ) -> VirtioGpuResult {
+        trace!("transfer_write ctx_id {ctx_id}, resource_id {resource_id}, {transfer:?}");
+
+        let resource = self
+            .resources
+            .get_mut(&resource_id)
+            .ok_or(ErrInvalidResourceId)?;
+
+        // FIXME: this is a horrible hack, that makes transfer_read in flush_resource work properly
+        // without this workaround partial transfer_write (not the whole area of the screen) seems
+        // to produce mostly black / empty images
+        if let Some(scanout_id) = resource.scanouts.iter_enabled().next() {
+            let rect = &mut self.scanouts[scanout_id as usize].as_mut().unwrap().rect;
+            transfer.x = 0;
+            transfer.y = 0;
+            transfer.w = rect.width;
+            transfer.h = rect.height;
+        }
+
         self.rutabaga
             .transfer_write(ctx_id, resource_id, transfer)?;
         Ok(OkNoData)
